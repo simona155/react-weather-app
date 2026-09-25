8d9f5671ed4dce76e486986725eff3df786710d9
Simona <simona.georgieva@students.finki.ukim.mk>
2026-09-25 16:22:59 +0200

    Fix weather map marker and location

--- src/components/WeatherMap.jsx
+++ src/components/WeatherMap.jsx
@@ -1,5 +1,30 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const defaultIcon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIconRetina,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+L.Marker.prototype.options.icon = defaultIcon;
+
+function RecenterMap({ latitude, longitude }) {
+    const map = useMap();
+
+    map.setView([latitude, longitude]);
+
+    return null;
+}
 
 function WeatherMap({ latitude, longitude }) {
     return (
@@ -12,6 +37,11 @@ function WeatherMap({ latitude, longitude }) {
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <RecenterMap
+                latitude={latitude}
+                longitude={longitude}
             />
 
             <Marker position={[latitude, longitude]} />
